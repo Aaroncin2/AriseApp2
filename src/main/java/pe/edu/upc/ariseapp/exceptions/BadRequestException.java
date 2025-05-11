077dfab20ea6d0124336e3ae539e5b96a540b710
Pierogabulle <u20231E145@upc.edu.pe>
2025-05-11 18:51:37 -0500

    Modificación del app.properties para el despliegue

---
+++ src/main/java/pe/edu/upc/ariseapp/exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package pe.edu.upc.ariseapp.exceptions;
+
+public class BadRequestException  extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
