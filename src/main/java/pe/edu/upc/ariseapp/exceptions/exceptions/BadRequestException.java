72f2544736fc20122d49a9b92a3ad58e9456e8ab
Pierogabulle <u20231E145@upc.edu.pe>
2025-05-11 19:04:11 -0500

    Solución de errores para el despliegue 19:04

--- src/main/java/pe/edu/upc/ariseapp/exceptions/exceptions/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.ariseapp.exceptions.exceptions;
-
-public class BadRequestException  extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
--- src/main/java/pe/edu/upc/ariseapp/exceptions/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.ariseapp.exceptions.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String mensaje) {
-        super(mensaje);
-    }
-}
